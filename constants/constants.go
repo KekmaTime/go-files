b26c43101f7327a20f2f6f7262dd6b3bfab44db0
KekmaTime <ananthbiji@gmail.com>
2023-06-25 00:53:21 +0530

    constants

---
+++ constants/constants.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const PremiumPlan = "Premium Membership"
+	const StandardPlan = "Standard Membership"
+
+	fmt.Printf("PremiumPlan: %v\n", PremiumPlan)
+	fmt.Printf("Standardplan: %v\n", StandardPlan)
+}
